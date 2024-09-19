52adfe181997af2b494a403f527884e2fcd0cefa
Antonio2209 <luisantonioramirez583@gmail.com>
2024-09-19 14:49:10 -0500

    menu de opciones

---
+++ menu.py
@@ -0,0 +1,17 @@
+#El menu de opciones
+# Menú interactivo
+def mostrar_menu():
+    print("\n--- Agencia de Viajes ---")
+    print("1. Crear paquete turístico")
+    print("2. Registrar cliente")
+    print("3. Hacer reserva")
+    print("4. Buscar cliente y mostrar reservas")
+    print("5. Buscar paquete turístico")
+    print("6. Mostrar todas las reservas")
+    print("7. Eliminar reserva")
+    print("8. Actualizar paquete turístico")
+    print("9. Mostrar paquetes turísticos")
+    print("10. Eliminar paquete turístico")
+    print("11. Actualizar cliente")
+    print("12. Eliminar cliente")
+    print("13. Salir")
