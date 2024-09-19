7385549c9212a5a4dfc58d19ac42cefa2deef320
Antonio2209 <luisantonioramirez583@gmail.com>
2024-09-19 13:34:21 -0500

    Dicionarios de paquete, clientes, hoteles

---
+++ Diccionarios.py
@@ -0,0 +1,54 @@
+paquetes = {
+    "Aventura en la Selva": {
+        "nombre": "Aventura en la Selva",
+        "descripcion": "Un paquete turístico que incluye senderismo y campamentos en la selva.",
+        "precio": "500"
+    },
+    "Escapada a la Playa": {
+        "nombre": "Escapada a la Playa",
+        "descripcion": "Vacaciones relajantes en una playa paradisiaca con actividades acuáticas.",
+        "precio": "800"
+    },
+    "Tour Histórico por Europa": {
+        "nombre": "Tour Histórico por Europa",
+        "descripcion": "Un recorrido por las ciudades históricas más importantes de Europa.",
+        "precio": "1500"
+    }
+}
+
+clientes = {
+    "Juan Perez": {
+        "nombre": "Juan Perez",
+        "correo": "juanperez@example.com",
+        "reservas": [
+            {
+                "tipo": "vuelo",
+                "destino": "Cancun",
+                "fecha": "2024-09-20"
+            }
+        ]
+    },
+    "Maria Gonzales": {
+        "nombre": "Maria Gonzales",
+        "correo": "mariagonalez@example.com",
+        "reservas": [
+            {
+                "tipo": "hotel",
+                "destino": "Paris",
+                "fecha": "2024-10-05"
+            },
+            {
+                "tipo": "vuelo",
+                "destino": "Nueva York",
+                "fecha": "2024-12-12"
+            }
+        ]
+    },
+    "Carlos Lopez": {
+        "nombre": "Carlos Lopez",
+        "correo": "carloslopez@example.com",
+        "reservas": []
+    }
+}
+# Opciones de hoteles predefinidos
+hoteles = ["Hotel Paris", "Resort Playa Azul", "Hostal Montaña", "Hotel Central City", "Hotel Luxor"]
